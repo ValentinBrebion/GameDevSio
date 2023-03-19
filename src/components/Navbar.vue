f35058f9501386e89c30189888e956ee9f210828
valentin <vb.brebionpro@gmail.com>
2023-03-19 23:01:14 +0100

    switch dark/light mode en cours avec vueuse

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+
+import { useDark, useToggle } from '@vueuse/core'
+const isDark = useDark()
+const toggleDark = useToggle(isDark)
+
+</script>
+
 <template>
     <nav class="bg-white px-2 sm:px-4 py-2.5 dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div class="container flex flex-wrap items-center justify-between mx-auto">
@@ -6,6 +14,13 @@
       <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">GamerCamp</span>
   </a>
   <div class="flex md:order-2">
+    <!--début toggle night day-->
+    <UseDark >
+  <button @click="toggleDark()">
+   nuit
+  </button>
+</UseDark>
+    <!--fin toggle night day-->
     <a href="/contact" class="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
       <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
         <span class="sr-only">Open main menu</span>
