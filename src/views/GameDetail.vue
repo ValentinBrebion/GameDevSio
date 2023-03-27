e58195f973fd77946dc0382aee83cf30a16a0650
valentin <vb.brebionpro@gmail.com>
2023-03-27 09:57:46 +0200

    changement id v-for

--- src/views/GameDetail.vue
+++ src/views/GameDetail.vue
@@ -64,13 +64,14 @@ fetchDetailGame()
                 Plateformes jouable
               </dt>
               <dd class="inline">
-                <div v-for="(Gaming, index) in Games" :key="Games.parent_platforms.platform.id">
+                <div v-for="(Gaming, index) in Games.parent_platforms.platform" :key="Gaming.parent_platforms.platform.id">
                     
-                    <div v-if="Games.parent_platforms.platform.name === 'PC'">
+                  <p>{{ Games.parent_platforms.platform.name }}</p>
+                    <!--<div v-if="Gaming.parent_platforms.platform.name === 'PC'">
                         <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9 17.25v1.007a3 3 0 01-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0115 18.257V17.25m6-12V15a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 15V5.25m18 0A2.25 2.25 0 0018.75 3H5.25A2.25 2.25 0 003 5.25m18 0V12a2.25 2.25 0 01-2.25 2.25H5.25A2.25 2.25 0 013 12V5.25"></path>
                         </svg>
-                    </div>
+                    </div>-->
                 </div>
               </dd>
             </div>
