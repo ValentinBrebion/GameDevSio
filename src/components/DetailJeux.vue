621d154ba065b8fca6a45c0058a55eae88c599e5
valentin <vb.brebionpro@gmail.com>
2023-04-06 19:44:32 +0200

    ajout description

--- src/components/DetailJeux.vue
+++ src/components/DetailJeux.vue
@@ -12,7 +12,7 @@
         <div class="lg:max-w-lg">
           <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
           <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl"><slot /></p>
-          <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+          <p class="mt-6 text-lg leading-8 text-gray-600"><slot2 /></p>
           <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
             <div class="relative pl-9">
               <dt class="inline font-semibold text-gray-900">
@@ -47,7 +47,7 @@
           </dl>
         </div>
       </div>
-      <img src="https://imgs.search.brave.com/SvNyvKjH0tqmClgeF3qabqib4DSkv8TKR0cgh1zVICI/rs:fit:770:770:1/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzMyL2Zl/LzBlLzMyZmUwZTJk/ZDlhNmE3MzdkNDM2/ZjdjNDA5NWNkMjk4/LnBuZw" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
+      <img src="e/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzMyL2Zl/LzBlLzMyZmUwZTJk/ZDlhNmE3MzdkNDM2/ZjdjNDA5NWNkMjk4/LnBuZw" class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0" width="2432" height="1442">
     </div>
   </div>
 </div>
--- src/views/GameDetail.vue
+++ src/views/GameDetail.vue
@@ -37,6 +37,7 @@ fetchDetailGame()
  <GameDetail>
  
   <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{{ Games.slug }}</p>
+  <p class="mt-6 text-lg leading-8 text-gray-600">{{ Games.description }}</p>
 
  </GameDetail>
                     
